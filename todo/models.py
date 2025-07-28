809dcad7a7251f0ad0f70c4d9888c177de1be886
2joonkim <joon271694@gmail.com>
2025-07-28 14:00:53 +0900

    ✨ 데일리 장고 업데이트

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
