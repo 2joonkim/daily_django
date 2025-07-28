8464fbe4dbebf19af125574c84bef7fb7cd805a5
2joonkim <joon271694@gmail.com>
2025-07-28 15:28:16 +0900

    ✨ 데일리 Django chapter_4 업데이트

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
 class Todo(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
     is_completed = models.BooleanField(default=False)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
